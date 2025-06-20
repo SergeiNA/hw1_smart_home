94cbfbbd317244c2415498b037dfa113beb017bf
SergeiNA <sergey.nikitin@mapbox.com>
2025-06-20 16:23:18 +0300

    wip: fix
    # Conflicts:
    #	.gitignore

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub mod smart_devices;
+mod smart_home;
+pub mod smart_room;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
